d18c91d6015be31a3d32cbe4a638eb78a3cc168d
Sir Dave <dabiola184@stu.ui.edu.ng>
2020-06-10 19:32:26 +0100

    Redux Thunk

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,17 +1,29 @@
 import React from "react";
 import {Card, CardBody, CardTitle, CardSubtitle, CardText, CardImg} from "reactstrap";
+import { Loading } from "./LoadingComponent";
 
-function RenderCard({item}){
-    return (
-        <Card>
-            <CardImg src={item.image} slt={item.name}/>
-            <CardBody>
-                <CardTitle><strong>{item.name}</strong></CardTitle>
-                {item.designation ? <CardSubtitle><strong>{item.designation}</strong></CardSubtitle> : null}
-                <CardText>{item.description}</CardText>
-            </CardBody>
-        </Card>
-    )
+function RenderCard({item, isLoading, errMess}){
+    if (isLoading){
+        return(
+            <Loading/>
+        )
+    }
+    else if (errMess){
+        return(
+            <h4>{errMess}</h4>
+        )
+    }
+    else
+        return (
+            <Card>
+                <CardImg src={item.image} slt={item.name}/>
+                <CardBody>
+                    <CardTitle><strong>{item.name}</strong></CardTitle>
+                    {item.designation ? <CardSubtitle><strong>{item.designation}</strong></CardSubtitle> : null}
+                    <CardText>{item.description}</CardText>
+                </CardBody>
+            </Card>
+        )
 }
 
 
@@ -21,7 +33,9 @@ function Home(props){
         <div className="container">
             <div className="row align-items-start">
                 <div className="col-12 col-md m-1">
-                    <RenderCard item={props.dish}/>
+                    <RenderCard item={props.dish}
+                    isLoading = {props.dishesLoading}
+                    errMess= {props.dishesErrMess}/>
                 </div>
                 <div className="col-12 col-md m-1">
                     <RenderCard item={props.promotion}/>
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from "reactstrap";
 import { Link } from "react-router-dom";
+import {Loading} from './LoadingComponent'
 
 function RenderMenuItem({dish, onClick}){
     return (
@@ -17,7 +18,22 @@ function RenderMenuItem({dish, onClick}){
 }
   
  const Menu = (props)=>{
-      const menu = props.dishes.map((dish) => {
+      const menu = props.dishes.dishes.map((dish) => {
+        if (props.dishes.isLoading){
+            return(
+                <Loading/>
+            )
+        }
+        else if (props.dishes.errMess){
+            return(
+                <div className="container">
+                    <div className="row">
+                        <h4>{props.dishes.errMess}</h4>
+                    </div>
+                </div>
+            )
+        }
+        else
             return (
               <div key={dish.id} className="col-12 col-md-5 m-1">
                  <RenderMenuItem dish={dish}/>
